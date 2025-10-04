40890a60207b6014126e330628530bacde77fd01
muizu <muizuvais99@gmail.com>
2025-10-04 14:21:03 +0530

    Refactor CategoryCard component for improved type definition and styling consistency

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
 import { getBackgroundColor } from "../../utils/style";
 
-type TCategoryCardProps = {
+type TProps = {
     category: string;
     label: string;
     href: string;
 };
-export function CategoryCard({ category, label, href }: TCategoryCardProps) {
+export function CategoryCard({ category, label, href }: TProps) {
     return (
         <Link
             href={href}
-            className={`flex items-center justify-center text-white font-semibold text-lg rounded-xl h-40 w-full sm:w-64 transition-transform duration-200 hover:scale-105 shadow-[10px_15px_20px_0px_#00000026] ${getBackgroundColor(
+            className={`flex items-center justify-center text-white font-semibold text-xl sm:text-2xl rounded-2xl min-h-72 sm:h-48 transition-transform duration-200 hover:scale-[1.02] shadow-[10px_15px_20px_0px_#00000026] ${getBackgroundColor(
                 category
             )}`}
+
         >
             {label}
         </Link>
     );
 }
+

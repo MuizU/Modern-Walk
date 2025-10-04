54ea9c3f6e6f2c6f5b9fb1d0c58587b79282c46a
muizu <muizuvais99@gmail.com>
2025-10-04 14:14:34 +0530

    Add ErrorPage component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+const ErrorPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-[#f5f5f5]">
+            <h6 className="text-3xl font-bold text-[#0E0E0E] mb-4">We couldn&#39;t find what you were looking for
+            </h6>
+            <p className="text-xl text-[#0E0E0E]">We are very sorry but something has gone wrong, please try again
+            </p>
+        </div>
+    );
+}
+
+
+export default ErrorPage;

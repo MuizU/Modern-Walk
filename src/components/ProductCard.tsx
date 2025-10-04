a351340342360029acf7aa274e660630dc2c8cc8
muizu <muizuvais99@gmail.com>
2025-10-04 14:47:03 +0530

    Update ProductCard component to use 'font-extrabold' for price text

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -22,7 +22,7 @@ const ProductCard = ({
                 <Image src={image} alt={title} width={100} height={100} />
             </div>
             <div className={`border border-transparent rounded-[15px] flex flex-col align-center justify-start  px-12 py-4 max-h-100 gap-2 ${getBackgroundColor(category)} mt-auto`}>
-                <p className="text-center text-lg font-bold text-[#0E42FD]">Rs {price}</p>
+                <p className="text-center text-lg font-extrabold text-[#0E42FD]">Rs {price}</p>
                 <p className="line-clamp-4 text-ellipsis text-sm h-20">{description}</p>
             </div>
         </div>

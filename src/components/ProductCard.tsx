7cf9b6de2d3d4124efcc48f78709bfc82134e005
muizu <muizuvais99@gmail.com>
2025-10-04 12:09:15 +0530

    Refactor ProductCard component for improved styling and layout

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { getBackgroundColor } from "../../utils/style";
 
 export type TProps = {
     category: `men's clothing` | `women's clothing`;
@@ -15,14 +16,14 @@ const ProductCard = ({
     image
 }: TProps) => {
     return (
-        <div className="bg-white w-2xs h-120">
-            <div className="flex flex-col align-center p-4 justify-center gap-2">
+        <div className="bg-white w-2xs h-100 rounded-lg flex flex-col card-shadow">
+            <div className="flex flex-col gap-2 items-center p-4 ">
                 <h6 className="text-center">{title}</h6>
-                <Image src={image} alt={title} width={200} height={200} />
+                <Image src={image} alt={title} width={100} height={100} />
             </div>
-            <div className="border border-transparent rounded-md flex flex-col justify-start  px-12 h-60 ">
-                <p>{price}</p>
-                <p className="multi-line-ellipsis text-sm h-20">{description}</p>
+            <div className={`border border-transparent rounded-lg flex flex-col align-center justify-start  px-12 py-4 max-h-100 gap-2 ${getBackgroundColor(category)} mt-auto`}>
+                <p className="text-center text-lg font-bold text-[#0E42FD]">Rs {price}</p>
+                <p className="line-clamp-4  text-ellipsis text-sm h-20">{description}</p>
             </div>
         </div>
     )

163ac40a1a16fbf92ec0588e9ebf223e8365afa3
muizu <muizuvais99@gmail.com>
2025-10-04 14:48:25 +0530

    Update CategoryCard component to increase font size for better visibility

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -10,7 +10,7 @@ export function CategoryCard({ category, label, href }: TProps) {
     return (
         <Link
             href={href}
-            className={`flex items-center justify-center text-white font-semibold text-xl sm:text-2xl rounded-2xl min-h-72 sm:h-48 transition-transform duration-200 hover:scale-[1.02] shadow-[10px_15px_20px_0px_#00000026] ${getBackgroundColor(
+            className={`flex items-center justify-center text-white font-semibold text-xl sm:text-5xl rounded-2xl min-h-72 sm:h-48 transition-transform duration-200 hover:scale-[1.02] shadow-[10px_15px_20px_0px_#00000026] ${getBackgroundColor(
                 category
             )}`}
 

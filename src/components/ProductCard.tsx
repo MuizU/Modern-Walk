e7899befd0e3bd3c98f5f4e6ed7d22893afe1d9c
muizu <muizuvais99@gmail.com>
2025-10-04 13:58:53 +0530

    Refactor ProductCard component for consistent styling and improved readability

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -16,14 +16,14 @@ const ProductCard = ({
     image
 }: TProps) => {
     return (
-        <div className="bg-white w-2xs h-100 rounded-lg flex flex-col card-shadow">
+        <div className="bg-white w-2xs h-100 rounded-[15px] flex flex-col card-shadow">
             <div className="flex flex-col gap-2 items-center p-4 ">
-                <h6 className="text-center">{title}</h6>
+                <h6 className="text-center font-bold">{title}</h6>
                 <Image src={image} alt={title} width={100} height={100} />
             </div>
-            <div className={`border border-transparent rounded-lg flex flex-col align-center justify-start  px-12 py-4 max-h-100 gap-2 ${getBackgroundColor(category)} mt-auto`}>
+            <div className={`border border-transparent rounded-[15px] flex flex-col align-center justify-start  px-12 py-4 max-h-100 gap-2 ${getBackgroundColor(category)} mt-auto`}>
                 <p className="text-center text-lg font-bold text-[#0E42FD]">Rs {price}</p>
-                <p className="line-clamp-4  text-ellipsis text-sm h-20">{description}</p>
+                <p className="line-clamp-4 text-ellipsis text-sm h-20">{description}</p>
             </div>
         </div>
     )

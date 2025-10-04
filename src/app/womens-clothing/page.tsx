72ee93ee05c280a83c1728c4dcc3e0f4f7149120
muizu <muizuvais99@gmail.com>
2025-10-04 14:15:59 +0530

    Add MensPage and WomensPage components for clothing category display

---
+++ src/app/womens-clothing/page.tsx
@@ -0,0 +1,32 @@
+import ProductCard from "@/components/ProductCard";
+import { getProducts } from "@/services/products"
+
+const WomensPage = async () => {
+    const productData = await getProducts() || [];
+    const mensClothing = productData.filter(item => item.category === `women's clothing`);
+    return (
+        <div className="font-sans min-h-screen px-8 sm:p-20 bg-[#f5f5f5]">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                <div className="flex flex-col gap-4 items-center sm:items-start w-full">
+                    <h3 className="text-3xl font-bold text-[#0E0E0E] p-4">
+                        Women&#39;s Clothing
+                    </h3>
+                    <div className="flex flex-row gap-10 justify-start flex-wrap">
+                        {mensClothing.map(item => (
+                            <ProductCard
+                                key={item.id}
+                                category={item.category as `men's clothing`}
+                                title={item.title}
+                                price={item.price}
+                                description={item.description}
+                                image={item.image}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default WomensPage;
